b55cc0ab146be79e9c128d6edc43e57e7f6a08ec
agohorel <alex.oddlogic@gmail.com>
2019-05-30 13:34:28 -0400

    add css reset

--- glitch-dot-cool/src/components/layout.js
+++ glitch-dot-cool/src/components/layout.js
@@ -2,32 +2,40 @@ import React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 
+// eslint-disable-next-line
 import styles from "../styles/global.css"
 import colors from "../styles/colors"
 
 const StyledLink = styled.li`
-    display: inline-block;
-    margin-right: 1rem;
+  display: inline-block;
+  margin-right: 1rem;
 
-    a, 
-    a:visited {
-        text-decoration: none;
-        color: ${colors.black};
-        transition: .2s ease all;
+  a,
+  a:visited {
+    text-decoration: none;
+    color: ${colors.black};
+    transition: 0.2s ease all;
 
-        :hover {
-            color: ${colors.lightgrey};
-        }
+    :hover {
+      color: ${colors.lightgrey};
     }
-
-`;
+  }
+`
 
 const Wrapper = styled.div`
-    margin: 3rem auto;
-    max-width: 67%;
-    padding: 0 5rem;
-`;
+  margin: 3rem auto;
+  max-width: 67%;
+  padding: 0 5rem;
+`
 
+const Footer = styled.div`
+  display: block;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 5rem;
+  background-color: ${colors.darkgrey};
+`
 
 const ListLink = props => (
   <StyledLink>
@@ -36,22 +44,22 @@ const ListLink = props => (
 )
 
 export default ({ children }) => (
-  <Wrapper>
-    <header style={{ marginBottom: `1.5rem` }}>
-      <StyledLink>
-        <Link to="/">
-          <h3>glitch[dot]cool</h3>
-        </Link>
-      </StyledLink>
-      <ul style={{ listStyle: `none`, float: `right` }}>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about/">About</ListLink>
-        <ListLink to="/contact/">Contact</ListLink>
-      </ul>
-    </header>
-    {children}
-    <h4 style={{ position: "absolute", bottom: "20px" }}>
-      copyright whatever whoever
-    </h4>
-  </Wrapper>
+  <div>
+    <Wrapper>
+      <header style={{ marginBottom: `1.5rem` }}>
+        <StyledLink>
+          <Link to="/">
+            <h3>glitch[dot]cool</h3>
+          </Link>
+        </StyledLink>
+        <ul style={{ listStyle: `none`, float: `right` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/contact/">Contact</ListLink>
+        </ul>
+      </header>
+      {children}
+    </Wrapper>
+    <Footer />
+  </div>
 )
