980600fa77cc07f9f7d18099db1d722e928bcc48
agohorel <alex.oddlogic@gmail.com>
2019-06-04 22:31:02 -0400

    create slugs from tags

--- glitch-dot-cool/gatsby-node.js
+++ glitch-dot-cool/gatsby-node.js
@@ -1,5 +1,21 @@
 const path = require(`path`)
 
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  let slugs = [];
+
+  if (node.internal.type === `ContentfulBlogPost`) {
+    node.tags.forEach(tag => {
+      slugs.push(slugify(tag));
+    })
+    createNodeField({
+      node,
+      name: `tagSlugs`,
+      value: slugs,
+    })
+  }
+}
+
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
@@ -27,4 +43,21 @@ module.exports.createPages = async ({ graphql, actions }) => {
       },
     })
   })
+}
+
+const slugify = string => {
+  const a = "àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;"
+  const b = "aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------"
+  const p = new RegExp(a.split("").join("|"), "g")
+
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
+    .replace(/\-\-+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, "") // Trim - from end of text
 }
