d09d5abac36a1faa26e603cd8ad203cb8066de95
agohorel <alex.oddlogic@gmail.com>
2019-06-09 12:59:11 -0400

    add clickable author link on /tags/:tag

--- glitch-dot-cool/src/templates/tag.js
+++ glitch-dot-cool/src/templates/tag.js
@@ -4,7 +4,8 @@ import { graphql } from "gatsby"
 // import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Layout from "../components/layout"
-import { StyledList, ListLink } from "../utils/utilComponents"
+import { StyledList, ListLink, StyledLink } from "../utils/utilComponents"
+import { slugify } from "../utils/utils"
 // import colors from "../styles/colors"
 // import measurements from "../styles/measurements"
 
@@ -16,6 +17,7 @@ export const query = graphql`
           tags
           slug
           title
+          author
           publishedDate(formatString: "MMMM Do, YYYY")
         }
       }
@@ -37,7 +39,15 @@ const Tag = props => {
                   <h2>{post.node.title}</h2>
                 </ListLink>
               </StyledList>
-              <p>{post.node.publishedDate}</p>
+              <p>
+                by{" "}
+                <StyledLink>
+                  <ListLink to={`/${slugify(post.node.author)}/posts`}>
+                    {post.node.author}
+                  </ListLink>
+                </StyledLink>
+                {` - ${post.node.publishedDate}`}
+              </p>
             </div>
           )
         })}
