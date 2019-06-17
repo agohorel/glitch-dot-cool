89e9b096abd0c2e834e5fc2f78dfa021e16b576e
agohorel <alex.oddlogic@gmail.com>
2019-06-17 13:28:40 -0400

    crop favicon to circle, add different formats, add link/meta tags to head

--- glitch-dot-cool/src/components/head.js
+++ glitch-dot-cool/src/components/head.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { Helmet } from "react-helmet"
 
-const Head = ({title}) => {
+const Head = ({ title }) => {
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -12,7 +12,31 @@ const Head = ({title}) => {
       }
     }
   `)
-  return <Helmet title={`${title} | ${data.site.siteMetadata.title}`} />
+  return (
+    <Helmet title={`${title} | ${data.site.siteMetadata.title}`}>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#191919" />
+      <meta name="msapplication-TileColor" content="#191919" />
+      <meta name="theme-color" content="#191919" />
+    </Helmet>
+  )
 }
 
 export default Head
