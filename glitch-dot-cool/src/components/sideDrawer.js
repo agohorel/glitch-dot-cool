43f0c169ba9d893bae103f7a023ee7ef35ad1cdf
agohorel <alex.oddlogic@gmail.com>
2019-07-25 22:45:39 -0400

    tone down drawer gradient

--- glitch-dot-cool/src/components/sideDrawer.js
+++ glitch-dot-cool/src/components/sideDrawer.js
@@ -9,8 +9,8 @@ const Drawer = styled.nav`
   height: 100%;
   background-image: linear-gradient(
     to bottom right,
-    ${colors.lightgrey},
-    ${colors.darkgrey}
+    ${colors.offwhite},
+    ${colors.midgrey}
   );
   box-shadow: -2px 0px 4px rgba(0, 0, 0, 0.2);
   position: fixed;
