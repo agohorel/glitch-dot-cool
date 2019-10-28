074faac6cfe5e8654d7eefe6741586f216d16adc
agohorel <alex.oddlogic@gmail.com>
2019-10-27 21:41:10 -0400

    move Card styled-component into utilComponent

--- glitch-dot-cool/src/components/Post.js
+++ glitch-dot-cool/src/components/Post.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 
-import { GatsbyLink } from "../utils/utilComponents"
+import { GatsbyLink, Card } from "../utils/utilComponents"
 import { slugify } from "../utils/utils"
 
 const Post = ({ post }) => {
@@ -24,28 +24,6 @@ const Post = ({ post }) => {
 }
 
 export default Post
-
-const Card = styled.div`
-  display: flex;
-  align-items: center;
-  justify-contents: center;
-  padding: 4rem;
-  height: 100%;
-
-  background-image: linear-gradient(
-      217deg,
-      rgba(200, 200, 200, 0.8),
-      rgba(200, 200, 200, 0) 70.71%
-    ),
-    linear-gradient(
-      127deg,
-      rgba(127, 127, 127, 0.8),
-      rgba(127, 127, 127, 0) 70.71%
-    ),
-    linear-gradient(336deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0) 70.71%);
-  background-size: cover;
-  background-position: center;
-`
 
 const TextContainer = styled.div`
   display: flex;
--- glitch-dot-cool/src/utils/utilComponents.js
+++ glitch-dot-cool/src/utils/utilComponents.js
@@ -119,6 +119,27 @@ const BlogImageSubtitle = styled.i`
   margin-bottom: 2rem;
 `
 
+const Card = styled.div`
+  display: flex;
+  align-items: center;
+  justify-contents: center;
+  padding: 4rem;
+  height: 100%;
+  background-image: linear-gradient(
+      217deg,
+      rgba(200, 200, 200, 0.8),
+      rgba(200, 200, 200, 0) 70.71%
+    ),
+    linear-gradient(
+      127deg,
+      rgba(127, 127, 127, 0.8),
+      rgba(127, 127, 127, 0) 70.71%
+    ),
+    linear-gradient(336deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0) 70.71%);
+  background-size: cover;
+  background-position: center;
+`
+
 export {
   GatsbyLink,
   StyledList,
@@ -130,4 +151,5 @@ export {
   BlogImageWrapper,
   BlogImageContainer,
   BlogImageSubtitle,
+  Card,
 }
