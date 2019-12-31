0b5d15b33fd86bd30bd108eb81b5e16925f27107
agohorel <alex.oddlogic@gmail.com>
2019-12-31 13:51:48 -0500

    refactor/simplify nav component

--- glitch-dot-cool/src/components/header.js
+++ glitch-dot-cool/src/components/header.js
@@ -1,7 +1,6 @@
 import React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
+import { Link } from "gatsby"
 import styled from "styled-components"
-
 
 import DrawerToggle from "./sideDrawerToggle"
 import { StyledList, GatsbyLink } from "../utils/utilComponents"
@@ -9,8 +8,48 @@ import { flicker, shifter } from "../styles/animations"
 import { activeNavStyles } from "../utils/utils"
 import measurements from "../styles/measurements"
 
+const Header = props => {
+  return (
+    <StyledHeader>
+      <StyledList>
+        <TextLogoWrapper>
+          <Link to="/">
+            <TextLogo>glitch[dot]cool</TextLogo>
+            <TextLogoShifted>glitch[dot]cool</TextLogoShifted>
+          </Link>
+        </TextLogoWrapper>
+      </StyledList>
+      <StyledNav>
+        <GatsbyLink to="/" activeStyle={activeNavStyles}>
+          home
+        </GatsbyLink>
+
+        <GatsbyLink to="/about/" activeStyle={activeNavStyles}>
+          about
+        </GatsbyLink>
+
+        <GatsbyLink to="/projects/" activeStyle={activeNavStyles}>
+          projects
+        </GatsbyLink>
+
+        <GatsbyLink to="/feeds/" activeStyle={activeNavStyles}>
+          feeds
+        </GatsbyLink>
+
+        <GatsbyLink to="/contact/" activeStyle={activeNavStyles}>
+          contact
+        </GatsbyLink>
+      </StyledNav>
+      <DrawerToggle click={props.drawerToggleClickHandler} />
+    </StyledHeader>
+  )
+}
+
+export { Header }
+
 const StyledHeader = styled.header`
   display: flex;
+  justify-content: space-between;
   height: 30px;
   padding: 6rem 0 3rem 0;
   transition: 0.2s ease-out padding;
@@ -24,10 +63,10 @@ const StyledNav = styled.nav`
   @media only screen and (max-width: ${measurements.breakpointMobileNav}px) {
     display: none;
   }
-`
 
-const Spacer = styled.div`
-  flex: 1;
+  a:not(:last-child) {
+    margin-right: 2rem;
+  }
 `
 
 const TextLogoWrapper = styled.div`
@@ -62,63 +101,3 @@ const TextLogoShifted = styled.h3`
   opacity: 0;
   pointer-events: none;
 `
-
-const Header = props => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `
-  )
-
-  return (
-    <StyledHeader>
-      <StyledList>
-        <TextLogoWrapper>
-          <Link to="/">
-            <TextLogo>{data.site.siteMetadata.title}</TextLogo>
-            <TextLogoShifted>{data.site.siteMetadata.title}</TextLogoShifted>
-          </Link>
-        </TextLogoWrapper>
-      </StyledList>
-      <Spacer />
-      <StyledNav>
-        <ul>
-          <StyledList>
-            <GatsbyLink to="/" activeStyle={activeNavStyles}>
-              home
-            </GatsbyLink>
-          </StyledList>
-          <StyledList>
-            <GatsbyLink to="/about/" activeStyle={activeNavStyles}>
-              about
-            </GatsbyLink>
-          </StyledList>
-          <StyledList>
-            <GatsbyLink to="/projects/" activeStyle={activeNavStyles}>
-              projects
-            </GatsbyLink>
-          </StyledList>
-          <StyledList>
-            <GatsbyLink to="/feeds/" activeStyle={activeNavStyles}>
-              feeds
-            </GatsbyLink>
-          </StyledList>
-          <StyledList>
-            <GatsbyLink to="/contact/" activeStyle={activeNavStyles}>
-              contact
-            </GatsbyLink>
-          </StyledList>
-        </ul>
-      </StyledNav>
-      <DrawerToggle click={props.drawerToggleClickHandler} />
-    </StyledHeader>
-  )
-}
-
-export { Header }
