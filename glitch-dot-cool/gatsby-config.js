3e5fb64432e755a78bff0613791a61716a8091f4
agohorel <gohorelalex@gmail.com>
2020-01-22 01:12:36 -0500

    fix icon path in manifest

--- glitch-dot-cool/gatsby-config.js
+++ glitch-dot-cool/gatsby-config.js
@@ -30,18 +30,7 @@ module.exports = {
         background_color: `#191919`,
         theme_color: `#e6e6e6`,
         display: `standalone`,
-        icons: [
-          {
-            src: "public/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "public/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
+        icon: `/android-chrome-512x512.png`,
       },
     },
     `gatsby-plugin-offline`,
