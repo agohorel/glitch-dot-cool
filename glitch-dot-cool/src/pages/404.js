4f03f67ec4179c3a118170c370710bcc45d79d28
agohorel <gohorelalex@gmail.com>
2020-01-22 23:21:07 -0500

    refactor 404 page

--- glitch-dot-cool/src/pages/404.js
+++ glitch-dot-cool/src/pages/404.js
@@ -1,23 +1,37 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Head from "../components/head"
-import { GatsbyLink, Centered } from "../utils/utilComponents"
+import { Centered, StyledLink } from "../utils/utilComponents"
 
 const NotFound = () => {
+  const goBack = e => {
+    e.preventDefault()
+    if (typeof window !== `undefined`) {
+      window.history.back()
+    }
+  }
+
   return (
     <Layout>
-      <Head title="404"/>
+      <Head title="404" />
       <Centered column style={{ marginTop: `25vh` }}>
         <h1>Sorry - this page doesn't exist.</h1>
         <p>but what does it mean to exist, anyway? you know?</p>
         <br />
-        <h2>
-          <GatsbyLink to="/">Return to Home page</GatsbyLink>
-        </h2>
+        <StyledLink onClick={goBack}>
+          <GoBack>Go Back</GoBack>
+        </StyledLink>
       </Centered>
     </Layout>
   )
 }
 
 export default NotFound
+
+const GoBack = styled.h2`
+  :hover {
+    cursor: pointer;
+  }
+`
