80b03491249c9d564f75aca2acbf95b6928d0f37
agohorel <3317203+agohorel@users.noreply.github.com>
2020-08-17 10:14:09 -0400

    refactor Projects
    -break rendering into separate component (ProjectsList.js)
    -move all styles to bottom of component
    -simplify markup
    -tweak hover behavior/styles

---
+++ glitch-dot-cool/src/components/Projects/ProjectsList.js
@@ -0,0 +1,76 @@
+import React from "react"
+import styled from "styled-components"
+import Image from "gatsby-image"
+
+import colors from "../../styles/colors"
+import Layout from "../layout"
+import Head from "../head"
+import { GatsbyLink, PageTitle } from "../../utils/utilComponents"
+
+const ProjectsList = ({ projects }) => {
+  return (
+    <Layout>
+      <Head title="projects" />
+      <PageTitle>projects</PageTitle>
+
+      {projects.map(post => {
+        return (
+          <GatsbyLink to={`/projects/${post.node.slug}`}>
+            <Post key={post.node.title}>
+              <ImageHover>
+                <Image style={imgStyles} fluid={post.node.artwork.fluid} />
+              </ImageHover>
+
+              <Textbox>
+                <h2>{post.node.title}</h2>
+                <p>{post.node.publishedDate}</p>
+              </Textbox>
+            </Post>
+          </GatsbyLink>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export default ProjectsList
+
+const Post = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 2rem;
+  background-color: #fff;
+  margin-top: 2rem;
+
+  :last-child {
+    margin-bottom: 2rem;
+  }
+
+  :hover {
+    background: ${colors.darkgrey};
+    color: ${colors.offwhite};
+  }
+
+  @media only screen and (max-width: 450px) {
+    flex-direction: column;
+  }
+`
+
+const Textbox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+`
+
+const ImageHover = styled.div`
+  transition: 0.2s ease filter;
+  :hover {
+    filter: invert(100%);
+  }
+`
+
+const imgStyles = {
+  width: `14rem`,
+  marginRight: `2rem`,
+  transition: `.2s ease all`,
+}
