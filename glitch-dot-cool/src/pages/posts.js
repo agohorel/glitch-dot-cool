04e9acae598b7266db3ed6ed49b750b417f5fb54
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-09-02 17:04:03 -0400

    rework /posts layout - use fluid grid to incr. density

--- glitch-dot-cool/src/pages/posts.js
+++ glitch-dot-cool/src/pages/posts.js
@@ -39,44 +39,70 @@ const Posts = () => {
       <Head title="posts" />
       <PageTitle>posts</PageTitle>
       <Filter setFilterTerm={setFilterTerm} path="/tags" />
-      {filterResult.map(post => {
-        const authors = parseAuthorLinks(post.node.author)
-        return (
-          <Post key={post.node.title}>
-            <GatsbyLink to={`/${slugify(authors[0].name)}/${post.node.slug}`}>
-              <h2>{post.node.title}</h2>
-              <p>{post.node.publishedDate}</p>
-              <p style={{ display: "inline" }}>{`by `}</p>
+      <PostContainer>
+        {filterResult.map(post => {
+          const authors = parseAuthorLinks(post.node.author)
+          return (
+            <Post key={post.node.title}>
+              <GatsbyLink to={`/${slugify(authors[0].name)}/${post.node.slug}`}>
+                <h2>{post.node.title}</h2>
+                <p>{post.node.publishedDate}</p>
+                <p style={{ display: "inline" }}>{`by `}</p>
 
-              {authors.map((author, idx) => {
-                if (idx < authors.length - 1) {
-                  return (
-                    <GatsbyLink to={author.slug}>
-                      <strong>{author.name},</strong>
-                    </GatsbyLink>
-                  )
-                } else {
-                  return (
-                    <GatsbyLink to={author.slug}>
-                      <strong>{author.name}</strong>
-                    </GatsbyLink>
-                  )
-                }
-              })}
-            </GatsbyLink>
-          </Post>
-        )
-      })}
+                {authors.map((author, idx) => {
+                  if (idx < authors.length - 1) {
+                    return (
+                      <GatsbyLink to={author.slug}>
+                        <strong>{author.name},</strong>
+                      </GatsbyLink>
+                    )
+                  } else {
+                    return (
+                      <GatsbyLink to={author.slug}>
+                        <strong>{author.name}</strong>
+                      </GatsbyLink>
+                    )
+                  }
+                })}
+              </GatsbyLink>
+            </Post>
+          )
+        })}
+      </PostContainer>
     </Layout>
   )
 }
 
 export default Posts
 
+const PostContainer = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  margin: 2rem 0 2rem 0;
+  grid-template-columns: repeat(auto-fit, minmax(32%, 1fr));
+
+  @media (max-width: 700px) {
+    grid-template-columns: repeat(auto-fit, minmax(50%, 1fr));
+  }
+
+  @media (max-width: 600px) {
+    grid-template-columns: repeat(auto-fit, minmax(100%, 1fr));
+  }
+`
+
 const Post = styled.div`
   padding: 2rem;
+  height: 100%;
   background-color: ${props => props.theme.colors.scale_6};
-  margin-top: 2rem;
+
+  h2 {
+    font-size: 2rem;
+  }
+
+  p,
+  a {
+    font-size: 1.6rem;
+  }
 
   :last-child {
     margin-bottom: 2rem;
