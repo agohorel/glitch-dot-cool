7784421da67e082fb855a47cd00bf53325a16868
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-09-02 19:01:32 -0400

    fix/improve dark mode link hovers, nav/footer colors

--- glitch-dot-cool/src/utils/utilComponents.js
+++ glitch-dot-cool/src/utils/utilComponents.js
@@ -9,10 +9,11 @@ export const GatsbyLink = styled(props => <Link to={props.to} {...props} />)`
   text-decoration: none;
   transition: 0.2s ease all;
   color: ${props =>
-    props.dark ? props.theme.colors.scale_5 : props.theme.colors.scale_1};
+    props.dark ? props.theme.colors.footer_text : props.theme.colors.scale_1};
 
   :hover {
-    color: ${props => props.theme.colors.footer_text_hover};
+    // color: ${props => props.theme.colors.footer_text_hover};
+    color: ${props => props.theme.colors.scale_3};
     ${chromaticAbberation}
   }
 `
